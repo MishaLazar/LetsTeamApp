cd6ef3ba902602a10f7e9e5316d8cc87c6dc1024
Misha Lazar <mishal@mail.afeka.ac.il>
2016-10-01 15:33:47 +0300

    michael chatroom project merge

---
+++ app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/ChatRoomGridSelectorFragment.java
@@ -0,0 +1,22 @@
+package com.misha.mor.letsteamapp.letsteamapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ChatRoomGridSelectorFragment extends Fragment {
+
+    public ChatRoomGridSelectorFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat_room_grid_selector, container, false);
+    }
+}
