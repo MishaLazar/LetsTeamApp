3276c6d164ad074d7905cae6b2a15e84041a3ce3
mor17_000 <mor171188@gmail.com>
2016-11-05 11:47:42 +0200

    added validator function to register activity

--- app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/Validator.java
+++ app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/Validator.java
@@ -1,7 +1,7 @@
 package com.misha.mor.letsteamapp.letsteamapp;
 
 /**
- * Created by mor17_000 on 11/4/2016.
+ * Created by mor on 11/4/2016.
  */
 
 public class Validator {
@@ -9,23 +9,24 @@ public class Validator {
     }
 
     public Boolean isValidEmail(String strEmailAddress){
-        if(strEmailAddress.matches(".*@.*\\..*") && strEmailAddress != null){
+        if((strEmailAddress.matches(".*@.*\\..*")) && (strEmailAddress != null)){
             return true;
         }
         return false;
     }
 
     public Boolean isValidName(String strName){
-        if(strName != null && strName.length() > 1){
+        if((strName != null) && (strName.length() > 1)){
             return true;
         }
         return false;
     }
 
     public Boolean isValidPassword(String strPassword){
-        if(strPassword != null && strPassword.length() > 5){
+        if((strPassword != null) && (strPassword.length() > 5)){
             return true;
         }
         return false;
     }
+
 }
