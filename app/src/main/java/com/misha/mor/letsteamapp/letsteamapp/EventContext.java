19716e09febabbf67fd8415ce76f228e83eb3b30
mor17_000 <mor171188@gmail.com>
2016-11-05 17:34:48 +0200

    added icons in event context

--- app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/EventContext.java
+++ app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/EventContext.java
@@ -5,17 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class EventContext extends AppCompatActivity {
 
-    EditText editTextContext;
-    EditText editTextName;
-    EditText editTextLocation;
+    TextView editTextContext;
+    TextView editTextName;
+    TextView editTextLocation;
 
-    Button btn_openEventChat;
-    Button btn_showEeventLocation;
+    ImageButton btn_openEventChat;
+    ImageButton btn_showEeventLocation;
     String userID;
     String eventID;
 
@@ -36,17 +37,17 @@ public class EventContext extends AppCompatActivity {
     }
 
     public void initViews(){
-        editTextContext = (EditText)findViewById(R.id.etxtContext);
+        editTextContext = (TextView)findViewById(R.id.etxtContext);
         editTextContext.setText("this is a demo data \n this is demo data \n this is demo data", TextView.BufferType.EDITABLE);
 
-        editTextName = (EditText)findViewById(R.id.etxtCreatorName);
+        editTextName = (TextView)findViewById(R.id.etxtCreatorName);
         editTextName.setText("Mor and Misha", TextView.BufferType.EDITABLE);
 
-        editTextLocation = (EditText)findViewById(R.id.etxtLocation);
-        editTextLocation.setText("Afeka", TextView.BufferType.EDITABLE);
+        editTextLocation = (TextView)findViewById(R.id.etxtLocation);
+        editTextLocation.setText("ספיר 7, הרצליה", TextView.BufferType.EDITABLE);
 
 
-        btn_openEventChat = (Button)findViewById(R.id.btnOpenChat);
+        btn_openEventChat = (ImageButton)findViewById(R.id.btnOpenChat);
         if(btn_openEventChat != null){
 
             btn_openEventChat.setOnClickListener(new View.OnClickListener() {
@@ -62,15 +63,16 @@ public class EventContext extends AppCompatActivity {
 
         }
 
-        btn_showEeventLocation = (Button)findViewById(R.id.btnShowEventLocation);
+        btn_showEeventLocation = (ImageButton)findViewById(R.id.btnShowEventLocation);
         if(btn_showEeventLocation != null){
 
             btn_showEeventLocation.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     //TODO: get real location to put in intent
+                    String strEventLocation = editTextLocation.getText().toString();
                     Intent intent = new Intent(EventContext.this, MapsActivity.class);
-                    intent.putExtra("eventLocation","");
+                    intent.putExtra("eventLocation",strEventLocation);
                     startActivity(intent);
 
                 }
