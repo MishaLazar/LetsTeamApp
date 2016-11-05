9523e3442310b1efbb906518fb5840cd07e8d763
mor17_000 <mor171188@gmail.com>
2016-11-05 11:14:33 +0200

    added validator class

---
+++ app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/Validator.java
@@ -0,0 +1,31 @@
+package com.misha.mor.letsteamapp.letsteamapp;
+
+/**
+ * Created by mor17_000 on 11/4/2016.
+ */
+
+public class Validator {
+    public Validator() {
+    }
+
+    public Boolean isValidEmail(String strEmailAddress){
+        if(strEmailAddress.matches(".*@.*\\..*") && strEmailAddress != null){
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean isValidName(String strName){
+        if(strName != null && strName.length() > 1){
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean isValidPassword(String strPassword){
+        if(strPassword != null && strPassword.length() > 5){
+            return true;
+        }
+        return false;
+    }
+}
