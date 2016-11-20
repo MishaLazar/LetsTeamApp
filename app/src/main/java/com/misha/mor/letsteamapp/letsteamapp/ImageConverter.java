795c81e608ae4bb736baaa0b1c9c68b8ce8ca171
Misha Lazar <mishal@mail.afeka.ac.il>
2016-11-20 19:43:21 +0200

    Image Test

---
+++ app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/ImageConverter.java
@@ -0,0 +1,145 @@
+package com.misha.mor.letsteamapp.letsteamapp;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Misha on 11/18/2016.
+ */
+
+public class ImageConverter {
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = pixels;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
+
+    public static Bitmap download_Image(String userImageURL) {
+        Bitmap bmp = null;
+        try {
+            URL urln = new URL(userImageURL);
+            HttpURLConnection con = (HttpURLConnection) urln.openConnection();
+            InputStream is = con.getInputStream();
+            bmp = BitmapFactory.decodeStream(is);
+        } catch (Exception e) {
+             e.toString();
+        }
+        return bmp;
+    }
+
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {
+        if (bitmap == null)
+            return null;
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        int color = Color.RED;
+        Paint paint = new Paint();
+        Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        RectF rectF = new RectF(rect);
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawOval(rectF, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    public static String BitMapToString(Bitmap bitmap){
+        if (bitmap == null)
+            return null;
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] bytes = baos.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    public static Bitmap StringToBitMap(String encodedString){
+        if (encodedString == null)
+            return null;
+
+        try {
+            if (encodedString != null) {
+                byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+                return bitmap;
+            }
+        } catch(Exception e) {
+            e.getMessage();
+
+        }
+        return null;
+    }
+
+
+    public static Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        if (image == null)
+            return null;
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 0) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+
+    }
+/*
+    public static void setForceShowIcon(PopupMenu popupMenu) {
+        try {
+            Field[] fields = popupMenu.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                if ("mPopup".equals(field.getName())) {
+                    field.setAccessible(true);
+                    Object menuPopupHelper = field.get(popupMenu);
+                    Class<?> classPopupHelper = Class.forName(menuPopupHelper
+                            .getClass().getName());
+                    Method setForceIcons = classPopupHelper.getMethod(
+                            "setForceShowIcon", boolean.class);
+                    setForceIcons.invoke(menuPopupHelper, true);
+                    break;
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }*/
+}
--- app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/RegisterActivity.java
+++ app/src/main/java/com/misha/mor/letsteamapp/letsteamapp/RegisterActivity.java
@@ -1,16 +1,27 @@
 package com.misha.mor.letsteamapp.letsteamapp;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,14 +31,31 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class RegisterActivity extends AppCompatActivity {
+    static final int CAMERA_PHOTO = 111;
+    final int MY_PERMISSION_WRITE_EXTERNAL = 90;
+    final int MY_PERMISSION_LOCATION = 99;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    final int PIC_CROP = 1;
+    String mCurrentPhotoPath;
+    Uri imageToUploadUri;
 
     Intent intent;
     EditText etxtUser;
     EditText etxtPass;
     EditText etxtEmail;
     Button btnSignUp;
+    Button btnCamera;
+    ImageView mImageView;
+    ImageView mImageView2;
 
     //vars
     String sUsername;
@@ -35,6 +63,7 @@ public class RegisterActivity extends AppCompatActivity {
     String sUserEmail;
     String uid;
     User newUser;
+    Uri photoURI;
 
     //db
     FireBaseDAL fdb; //DAL
@@ -42,6 +71,7 @@ public class RegisterActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
     SharedPreferences sharedPreferences;
+    Bitmap reducedSizeBitmap;
 
 
     @Override
@@ -51,6 +81,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         initViews();
+        checkPermission(MY_PERMISSION_LOCATION);
 
         //sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         sharedPreferences = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
@@ -59,6 +90,19 @@ public class RegisterActivity extends AppCompatActivity {
         fdb.setContext(RegisterActivity.this);
         mAuth = FirebaseAuth.getInstance();
 
+
+
+    }
+
+    public void initViews(){
+        etxtUser = (EditText)findViewById(R.id.etxtUserName);
+
+        etxtPass = (EditText)findViewById(R.id.etxtPassword);
+
+        etxtEmail = (EditText)findViewById(R.id.etxtEmail);
+
+        mImageView = (ImageView)findViewById(R.id.imageView);
+        mImageView2 = (ImageView)findViewById(R.id.imageView2);
 
         btnSignUp = (Button)findViewById(R.id.btnSignUp);
         if(btnSignUp != null){
@@ -97,14 +141,48 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             });
         }
+
+        btnCamera = (Button)findViewById(R.id.btnCamera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkPermission(MY_PERMISSION_WRITE_EXTERNAL)) {
+//                    dispatchTakePictureIntent();
+                    captureCameraImage();
+                }
+                setPic();
+                /*galleryAddPic();*/
+
+            }
+        });
+
+
     }
 
-    public void initViews(){
-        etxtUser = (EditText)findViewById(R.id.etxtUserName);
+    private boolean checkPermission(int my_permission_code) {
+        switch (my_permission_code) {
+            case MY_PERMISSION_WRITE_EXTERNAL: {
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    /*Log.i(TAG, "No write permission, requesting permission");*/
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_WRITE_EXTERNAL);
+                    return false;
+                }
+            }
 
-        etxtPass = (EditText)findViewById(R.id.etxtPassword);
+            case MY_PERMISSION_LOCATION: {
+                if (ContextCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                   /* Log.i(TAG, "No location permission, requesting permission");*/
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_PERMISSION_LOCATION);
+                    return false;
+                }
+            }
+        }
+        return true;
 
-        etxtEmail = (EditText)findViewById(R.id.etxtEmail);
     }
 
     public Boolean isUserRegistrationValid(){
@@ -140,6 +218,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         newUser = new User(sUsername,uid,sPassword, sUserEmail);
         if(newUser != null){
+            newUser.setBitMapUserImage(ImageConverter.BitMapToString(reducedSizeBitmap));
             fdb.registerUser(newUser);
         }
 
@@ -187,4 +266,190 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    //initiate the external camera app
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+               /* ...*/
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+    private void captureCameraImage() {
+        Intent chooserIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File f = new File(Environment.getExternalStorageDirectory(), "POST_IMAGE.jpg");
+        chooserIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        imageToUploadUri = Uri.fromFile(f);
+        startActivityForResult(chooserIntent, CAMERA_PHOTO);
+    }
+    //receive the image after
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try{
+
+                *//*Bundle extras = data.getExtras();*//*
+                Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoURI);
+                *//*mImageView.setImageBitmap(imageBitmap);*//*
+                mImageView.setImageBitmap(RoundedImageView.getCroppedBitmap(imageBitmap,40));
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_PHOTO && resultCode == Activity.RESULT_OK) {
+            if(imageToUploadUri != null){
+                Uri selectedImage = imageToUploadUri;
+                getContentResolver().notifyChange(selectedImage, null);
+                reducedSizeBitmap = getBitmap(imageToUploadUri.getPath());
+                if(reducedSizeBitmap != null){
+                    mImageView.setImageBitmap(ImageConverter.getRoundedCornerBitmap(reducedSizeBitmap,60));
+                    /*mImageView.setImageBitmap(RoundedImageView.getCroppedBitmap(reducedSizeBitmap,90));*/
+                }else{
+                    Toast.makeText(this,"Error while capturing Image",Toast.LENGTH_LONG).show();
+                }
+            }else{
+                Toast.makeText(this,"Error while capturing Image",Toast.LENGTH_LONG).show();
+            }
+        }
+        if (requestCode == PIC_CROP) {
+            if (data != null) {
+                // get the returned data
+                Bundle extras = data.getExtras();
+                // get the cropped bitmap
+                Bitmap selectedBitmap = extras.getParcelable("data");
+
+                mImageView2.setImageBitmap(selectedBitmap);
+            }
+        }
+    }
+    //create unique file name to store the image
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    //add image taken to gallery dir
+   /* private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }*/
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+        mImageView.setImageBitmap(bitmap);
+    }
+    private Bitmap getBitmap(String path) {
+
+        Uri uri = Uri.fromFile(new File(path));
+        InputStream in = null;
+        try {
+            final int IMAGE_MAX_SIZE = 1200000; // 1.2MP
+            in = getContentResolver().openInputStream(uri);
+
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, null, o);
+            in.close();
+
+
+            int scale = 1;
+            while ((o.outWidth * o.outHeight) * (1 / Math.pow(scale, 2)) >
+                    IMAGE_MAX_SIZE) {
+                scale++;
+            }
+            Log.d("", "scale = " + scale + ", orig-width: " + o.outWidth + ", orig-height: " + o.outHeight);
+
+            Bitmap b = null;
+            in = getContentResolver().openInputStream(uri);
+            if (scale > 1) {
+                scale--;
+                // scale to max possible inSampleSize that still yields an image
+                // larger than target
+                o = new BitmapFactory.Options();
+                o.inSampleSize = scale;
+                b = BitmapFactory.decodeStream(in, null, o);
+
+                // resize to desired dimensions
+                int height = b.getHeight();
+                int width = b.getWidth();
+                Log.d("", "1th scale operation dimenions - width: " + width + ", height: " + height);
+
+                double y = Math.sqrt(IMAGE_MAX_SIZE
+                        / (((double) width) / height));
+                double x = (y / height) * width;
+
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(b, (int) x,
+                        (int) y, true);
+                b.recycle();
+                b = scaledBitmap;
+
+                System.gc();
+            } else {
+                b = BitmapFactory.decodeStream(in);
+            }
+            in.close();
+
+            Log.d("", "bitmap size - width: " + b.getWidth() + ", height: " +
+                    b.getHeight());
+            return b;
+        } catch (IOException e) {
+            Log.e("", e.getMessage(), e);
+            return null;
+        }
+    }
+
 }
